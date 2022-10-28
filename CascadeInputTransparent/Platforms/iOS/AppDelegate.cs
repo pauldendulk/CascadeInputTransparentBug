bb354553994c349ee7362e7e2f568b6d1928e55e
pauldendulk <pauldendulk@gmail.com>
2022-10-28 14:50:41 +0200

    Add entire solution

---
+++ CascadeInputTransparent/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CascadeInputTransparent;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
